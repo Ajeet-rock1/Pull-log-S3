250a78d56ca6f96f0d4d621202d2197dc6947275
Ajeet <Ajeet-rock1@users.noreply.github.com>
2018-04-23 09:06:54 +0530

    Update AWS-ELB.sh

--- AWS-ELB.sh
+++ AWS-ELB.sh
@@ -2,7 +2,7 @@
 year=`date +%Y`
 month=`date +%m`
 day=`date +%d`
-filetopull=`/usr/local/bin/s3cmd ls s3://elb-logs-1/ELB/elb1/AWSLogs/832198632263/elasticloadbalancing/ap-south-1/$year/$month/$day/* |  tail -1 | awk '{print $4}'`
+filetopull=`/usr/local/bin/s3cmd ls s3://elb-logs-1/ELB/elb1/AWSLogs/87288636263/elasticloadbalancing/ap-south-1/$year/$month/$day/* |  tail -1 | awk '{print $4}'`
 fileforgunzip=`basename $filetopull`
 finalfile=`basename $filetopull`
 cd /opt/elk/
@@ -10,14 +10,14 @@ cd /opt/elk/
 /bin/cp -a /opt/elk/*.log /var/log/elb/elb1.log
 rm -rf /opt/elk/*.log
 
-filetopull1=`/usr/local/bin/s3cmd ls s3://elb-logs-2/ELB/elb2/AWSLogs/832198632263/elasticloadbalancing/ap-south-1/$year/$month/$day/* |  tail -1 | awk '{print $4}'`
+filetopull1=`/usr/local/bin/s3cmd ls s3://elb-logs-2/ELB/elb2/AWSLogs/87288636263/elasticloadbalancing/ap-south-1/$year/$month/$day/* |  tail -1 | awk '{print $4}'`
 file1=`basename $filetopull1`
 cd /opt/elk/
 /usr/local/bin/s3cmd get $filetopull1
 /bin/cp -a /opt/elk/*.log /var/log/elb/elb2.log
 rm -rf /opt/elk/*.log
 
-filetopull2=`/usr/local/bin/s3cmd ls s3://elb-logs-3/ELB/elb3/AWSLogs/832198632263/elasticloadbalancing/ap-south-1/$year/$month/$day/* |  tail -1 | awk '{print $4}'`
+filetopull2=`/usr/local/bin/s3cmd ls s3://elb-logs-3/ELB/elb3/AWSLogs/87288636263/elasticloadbalancing/ap-south-1/$year/$month/$day/* |  tail -1 | awk '{print $4}'`
 file2=`basename $filetopull2`
 cd /opt/elk/
 /usr/local/bin/s3cmd get $filetopull2
@@ -26,7 +26,7 @@ rm -rf /opt/elk/*.log
 
 
 
-filetopull3=`/usr/local/bin/s3cmd ls s3://elb-logs-4/ELB/elb4/AWSLogs/832198632263/elasticloadbalancing/ap-south-1/$year/$month/$day/* |  tail -1 | awk '{print $4}'`
+filetopull3=`/usr/local/bin/s3cmd ls s3://elb-logs-4/ELB/elb4/AWSLogs/87288636263/elasticloadbalancing/ap-south-1/$year/$month/$day/* |  tail -1 | awk '{print $4}'`
 file3=`basename $filetopull3`
 cd /opt/elk/
 /usr/local/bin/s3cmd get $filetopull3
